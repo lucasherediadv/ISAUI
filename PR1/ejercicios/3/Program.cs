ac5eb96cd74bbbaa755113bb8574626a10bb7e9c
Lucas Heredia <lucasherediadv@gmail.com>
2025-09-03 15:28:58 -0300

    Agregando resolucion de ejercicio

---
+++ PR1/ejercicios/3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class TotalVentasVendedor
+{
+    public static void Main(string[] args)
+    {
+        // Declara los dos vectores de 10 elementos
+        int[] codigoVendedor = new int[10];
+        decimal[] importeVenta = new decimal[10];
+
+        Console.WriteLine("---Carga de ventas ---");
+
+        // Carga los vectores con los datos ingresados por teclado
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"\nRegistro de venta N° {i + 1}");
+
+            // Solicitar y guardar codigo del vendedor
+            Console.Write("ingrese el codigo del vendedor: ");
+            if (!int.TryParse(Console.ReadLine(), out codigoVendedor[i]))
+            {
+                Console.WriteLine("Error: Ingrese un numero entero valido.");
+                return;
+            }
+
+            // Pide i guarda el importe de la venta
+            Console.Write("ingrese el importe de la venta: ");
+            if (!decimal.TryParse(Console.ReadLine(), out importeVenta[i]))
+            {
+                Console.WriteLine("Error: Ingrese un numero valido para el importe");
+                return;
+            }
+        }
+
+        // Pide el codigo del venededor a buscar y calcula el total
+        Console.WriteLine("\n--- Busqueda de ventas ---");
+        Console.WriteLine("Ingrese el codigo de vendedor a buscar: ");
+        int codigoBuscado;
+        if (!int.TryParse(Console.ReadLine(), out codigoBuscado))
+        {
+            Console.WriteLine("Error: Ingrese un numero entero valido.");
+            return;
+        }
+
+        decimal totalVentas = 0;
+
+        // Recorre los vectores para sumar todas las ventas del vendedor
+        // buscado
+        for (int i = 0; i < 10; i++)
+        {
+            if (codigoVendedor[i] == codigoBuscado)
+            {
+                totalVentas += importeVenta[i];
+            }
+        }
+
+        Console.WriteLine($"\nEl total de ventas para el vendedor {codigoBuscado} es: ${totalVentas}");
+    }
+}
---
+++ PR1/ejercicios/3/README.md
@@ -0,0 +1,3 @@
+# Enunciado
+
+Cargar dos vectores de 10 elementos cada uno, teniendo en cuenta que el primer vector debe tener el código del vendedor y el segundo vector debe tener el importe de la venta realizada (Un vendedor puede tener uno o más ventas). Una vez finalizada la carga debe ingresar por teclado el código del vendedor y calcular el total general de ventas realizadas.
